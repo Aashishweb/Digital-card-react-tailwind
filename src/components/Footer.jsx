591f9661ed9177ddb5a3a97c6eba1f423ebe6e9d
Ashish <102414529+Aashishweb@users.noreply.github.com>
2023-01-08 19:56:10 +0530

    file path change for all images again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer(){
     return(
       <footer className="flex flex-row justify-center items-center w-full  bottom-0 fixed  max-w-sm  p-5   bg-Cinder gap-5 ">
-        <a href="http://www.facebook.com"><img  src="/assets/FacebookIcon.svg" alt="facebook logo" /></a>
-        <a href='http://www.github.com'> <img src="/assets/GitHubIcon.svg" alt="Github logo" /></a>
-        <a href="http://www.instagram.com"><img src="/assets/InstagramIcon.svg" alt="Instagram logo" /></a>
-        <a href="http://www.twitter.com"><img src="/assets/TwitterIcon.svg" alt="Twitter logo" /></a>
+        <a href="http://www.facebook.com"><img  src="/FacebookIcon.svg" alt="facebook logo" /></a>
+        <a href='http://www.github.com'> <img src="/GitHubIcon.svg" alt="Github logo" /></a>
+        <a href="http://www.instagram.com"><img src="/InstagramIcon.svg" alt="Instagram logo" /></a>
+        <a href="http://www.twitter.com"><img src="/TwitterIcon.svg" alt="Twitter logo" /></a>
       </footer> 
     )
 }
